1dacf0970150e6e3addc52c2217930ae30d441b8
Viosagmirt <viosagmirt@gmail.com>
2024-03-30 20:01:48 +0300

    Added Admin Controller v1

---
+++ UserProject_4M/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserProject_4M.Models;
+
+namespace UserProject_4M.Controllers
+{
+	public class AdminController : Controller
+    {
+		private UserManager<AppUser> userManager;
+
+		public AdminController(UserManager<AppUser> userManager)
+		{
+			this.userManager = userManager;
+		}
+
+		public IActionResult Index()
+		{
+			return View(userManager.Users);
+		}
+    }
+}
+
---
+++ UserProject_4M/Models/UserViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserProject_4M.Models
+{
+	public class CreateViewModel
+    {
+		[Required]
+		public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+	}
+
+	public class SecondViewModel
+	{
+
+	}
+}
+
